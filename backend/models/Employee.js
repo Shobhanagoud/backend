fe2d2819c5f5e40b1e7195ecb7d035944a3f0eb9
shobhana <shobhanakolukonda@gmail.com>
2024-11-09 10:49:42 +0530

    deploy my backend

---
+++ backend/models/Employee.js
@@ -0,0 +1,21 @@
+const mongoose=require('mongoose')
+
+const employeeSchema=new mongoose.Schema({
+   name:{
+      type: String,
+      required: true
+   },
+   email:{
+    type: String,
+    required: true
+   },
+   phone:{
+    type:Number,
+    required:true
+   },
+   city:{
+    type: String,
+    required:true
+   }
+})
+module.exports=mongoose.model("Employee",employeeSchema);
